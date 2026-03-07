5cf9ef0adce11204ce38f30216e7608e61bab2c4
wangly <wangleungy77@gmail.com>
2026-03-07 00:06:31 -0800

    update cmake and run-comands

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include <boost/program_options.hpp>
 #include <fstream>
+#include <cstdio>
 #include "distancemat.hpp"
-#include "matcal.cpp"
+#include "buildtree.hpp"
 #include "timer.hpp"
 
 namespace po = boost::program_options;
@@ -10,11 +11,13 @@ namespace po = boost::program_options;
 int main(int argc, char** argv) {
     Timer timer;
     std::string matrixFilename;
+    std::string outputFilename;
     uint32_t maxTaxa;
 
     po::options_description desc{"Options"};
     desc.add_options()
     ("distanceMatrix,m", po::value<std::string>(&matrixFilename)->required(), "Input distance matrix in PHYLIP format [REQUIRED].")
+    ("output,o", po::value<std::string>(&outputFilename), "Output file in the Newick/New-Hampshire format.")
     ("maxTaxa,N", po::value<uint32_t>(&maxTaxa)->default_value(1000), "Maximum number of taxa to read from the distance matrix file.")
     ("help,h", "Print help messages");
 
@@ -36,9 +39,9 @@ int main(int argc, char** argv) {
         fprintf(stderr, "ERROR: Cannot open file: %s\n", matrixFilename.c_str());
     }
     
-    DistanceMatrix mat = readPhylipDistanceMatrix(fp); 
-    DistanceMatrix computed_tree = compute_tree(mat);
-    printDistanceMatrix(mat);
+    // DistanceMatrix mat = readPhylipDistanceMatrix(fp); 
+    // DistanceMatrix computed_tree = compute_tree(mat);
+    // printDistanceMatrix(mat);
 
     std::vector<std::string> identifiers;
     
@@ -46,4 +49,18 @@ int main(int argc, char** argv) {
     fprintf(stdout, "Reading distance matrix from file.\n");
     DistanceMatrix mat = readPhylipDistanceMatrix(fp, identifiers);
     fprintf(stdout, "Completed in %ld msec \n\n", timer.Stop());
+
+    FILE* out = stdout;
+
+    if (!outputFilename.empty()) {
+        out = fopen(outputFilename.c_str(), "w");
+        if (!out) {
+            fprintf(stderr, "ERROR: cant open file: %s\n", outputFilename.c_str());
+            exit(1);
+        }
+    }
+
+    Tree* myTree = neighbour_joining_buildtree(&mat, identifiers);
+    write_newhampshire_Tree(out, myTree);
+
 }
